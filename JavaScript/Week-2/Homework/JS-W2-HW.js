a8921747cb6aea37cdfa671106b527f8b76404b7
PuertoPounder <rodriquezap@gmail.com>
2024-10-10 18:14:37 -0600

    Java Script Week 2 HW
    Did the tip calculator h/w

--- JavaScript/Week-2/Homework/JS-W2-HW.js
+++ JavaScript/Week-2/Homework/JS-W2-HW.js
@@ -1,8 +1,21 @@
 // TODO: Function to calculate tip and total amount
+function calculateTip(){
+    const billAmount = parseFloat(document.getElementById('bill-amount').value);
+    const tipPercentage = parseFloat(document.getElementById('tip-percentage').value);
 
     // TODO: Get the user input values
+    if (isNaN(billAmount) || isNaN(tipPercentage) || billAmount <= 0 || tipPercentage < 0) {
+        alert('Please enter in valid numbers.');
+        return;
+    }
     // TODO: Validate the input
+    const tipAmount = billAmount * (tipPercentage / 100)
+    const totalAmount = billAmount + tipAmount;
     // TODO: Calculate the tip amount and total amount
+    document.getElementById('tip-amount').textContent = `Tip Amount: $${tipAmount.toFixed(2)}`;
+    document.getElementById('total-amount').textContent = `Total Amount: $${totalAmount.toFixed(2)}`
+}
     // TODO: Display the results
 
 // TODO: Add event listener to the HTML DOM button
+document.getElementById('calculate-button').addEventListener('click',calculateTip);
